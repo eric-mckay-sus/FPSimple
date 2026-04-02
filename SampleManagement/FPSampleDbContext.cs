c1198f429df324941bb08777ced30a8945822018
eric-mckay-sus <sus9584@stanley-electric.com>
2026-04-02 14:23:17 -0400

    Simple DB connection test working

--- SampleManagement/FPSampleDbContext.cs
+++ SampleManagement/FPSampleDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SampleManagement;
@@ -10,4 +9,53 @@ namespace SampleManagement;
 public class FPSampleDbContext(DbContextOptions<FPSampleDbContext> options) : DbContext(options)
 {
     // One set per table, MUST match table names
+    public DbSet<FoolproofEntry> FoolproofInfo { get; set; }
+    public DbSet<ModelLine> ModelToLine { get; set; }
+}
+
+[PrimaryKey(nameof(Model), nameof(Revision), nameof(Location), nameof(PartMasterNum))]
+public class FoolproofEntry
+{
+    [Column("model")]
+    public string Model { get; set; }
+
+    [Column("revision")]
+    public byte Revision { get; set; }
+
+    [Column("issueDate")]
+    public DateTime IssueDate { get; set; }
+
+    [Column("issuer")]
+    public string? Issuer { get; set; }
+
+    [Column("failureMode")]
+    public string FailureMode { get; set; }
+
+    [Column("rank")]
+    public string Rank { get; set; }
+
+    [Column("location")]
+    public string Location { get; set; }
+
+    [Column("partMasterNum")]
+    public short PartMasterNum { get; set; }
+}
+
+[PrimaryKey(nameof(IcsNum), nameof(WorkCenterCode))]
+public class ModelLine
+{
+    [Column("icsNum")]
+    public string IcsNum { get; set; }
+
+    [Column("shortDesc")]
+    public string? ShortDescription { get; set; }
+
+    [Column("prodCellCode")]
+    public string? ProdCellCode { get; set; }
+
+    [Column("workCenterCode")]
+    public string WorkCenterCode { get; set; }
+
+    [Column("fullDesc")]
+    public string? FullDescription { get; set; }
 }
